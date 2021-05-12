4d2ccb10ad375a8a5bb0d9f32844e4fe49501ee9
Ramon Cardoso <96ddffab-b9f9-464c-87e4-c0a8e4e9c35d@mon.anonaddy.com>
2021-05-12 08:50:04 -0300

    Parte 5

--- DIO.Bank/Conta/Conta.cs
+++ DIO.Bank/Conta/Conta.cs
@@ -1,7 +1,39 @@
-namespace DIO.Bank.Conta
+using System;
+
+namespace DIO.Bank
 {
     public class Conta
     {
-        public string Nome { get; set; }
+        private string Nome { get; set; }
+        private double Saldo { get; set; }
+        private double Credito { get; set; }
+        private TipoConta TipoConta { get; set; }
+        public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+        {
+            this.TipoConta = tipoConta;
+            this.Saldo = saldo;
+            this.Credito = credito;
+            this.Nome = nome;
+        }
+        public bool Sacar(double valorSaque) 
+        {
+            if (this.Saldo - valorSaque < (this.Credito *-1))
+            {
+                Console.WriteLine("Saldo insuficiente!");
+                return false;
+            }
+            this.Saldo -= valorSaque;
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+            return true;
+        }
+        public void Depositar(double valorDeposito) {
+            this.Saldo += valorDeposito;
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+        }
+        public void Transferir(double valorTransferencia, Conta contaDestino) {
+            if(this.Sacar(valorTransferencia)){
+                contaDestino.Depositar(valorTransferencia);
+            }
+        }
     }
 }
